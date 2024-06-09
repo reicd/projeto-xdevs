4a06005fd5ca41798f06aaa6f4269f7e54cc3ca3
Reinaldo Duarte <62804743+reicd@users.noreply.github.com>
2024-06-09 19:27:01 -0300

    Projeto de uma semana
    Revisão

---
+++ src/js/index.js
@@ -0,0 +1,53 @@
+
+// const btnAvancar = document.getElementById("btn-avancar");
+// const btnVoltar = document.getElementById("btn-voltar");
+// let cartaoAtual = 0;
+// const cartoes = document.querySelectorAll(".cartao");
+// btnAvancar.addEventListener("click", function(){
+//     if(cartaoAtual===cartoes.length -1)return;
+//         const cartaoSelecionado = document.querySelector(".selecionado");
+//         cartaoSelecionado.remove("selecionado");
+//         cartaoAtual ++; 
+//         cartoes[cartaoAtual].classList.add("selecionado");
+//        });
+// btnVoltar.addEventListener("click", function(){
+//     if(cartaoAtual===0)return;    
+//     const cartaoSelecionado = document.querySelector(".selecionado");
+//         cartaoSelecionado.remove("selecionado");
+//         cartaoAtual --; 
+//         cartoes[cartaoAtual].classList.add("selecionado");
+// })
+
+const btnAvancar = document.getElementById("btn-avancar");
+const btnVoltar = document.getElementById("btn-voltar");
+let cartaoAtual = 0;
+const cartoes = document.querySelectorAll(".cartao");
+
+btnAvancar.addEventListener("click", function () {
+	const ehUltimoCartao = cartaoAtual === cartoes.length - 1;
+	if (ehUltimoCartao) return;
+
+	esconderCartaoSelecionado();
+
+	cartaoAtual++;
+	mostrarCartao();
+});
+
+btnVoltar.addEventListener("click", function () {
+	const ehPrimeiroCartao = cartaoAtual === 0;
+	if (ehPrimeiroCartao) return;
+
+	esconderCartaoSelecionado();
+
+	cartaoAtual--;
+	mostrarCartao();
+});
+
+function mostrarCartao() {
+	cartoes[cartaoAtual].classList.add("selecionado");
+}
+
+function esconderCartaoSelecionado() {
+	const cartaoSelecionado = document.querySelector(".selecionado");
+	cartaoSelecionado.classList.remove("selecionado");
+}
